ae55018912d1f8844bb266866778923ea57d7bfe
Heis97 <46742591+Heis97@users.noreply.github.com>
2022-11-10 11:26:01 +0300

    1011_1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1022,7 +1022,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          {15, 15,     5, 5, 5,     2, 2, 2 }
+#define DEFAULT_MAX_FEEDRATE          {25, 25,     15, 15, 15,     2, 2, 2 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
